d3075e07e1793c8d9bb2b4433fad9787afd6a1a6
Digis 01 <digis01Sistemas@outlook.com>
2026-03-19 11:18:43 -0600

    LoginPropio2.0

---
+++ src/main/java/com/digis01/DAraizaProgramacionNCapasMaven/Configuration/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.digis01.DAraizaProgramacionNCapasMaven.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author digis
+ */
+@Configuration
+public class SecurityConfiguration {
+    
+}
