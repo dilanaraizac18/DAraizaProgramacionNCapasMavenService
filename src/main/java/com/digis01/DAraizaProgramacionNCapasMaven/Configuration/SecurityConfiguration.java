99c3188cd7d1135cf8053b3e8fa1ca8d1b5a33e8
Dilan <Dilan@DESKTOP-12CB95S>
2026-03-23 16:03:32 -0600

    Security and authentication 2.0

--- src/main/java/com/digis01/DAraizaProgramacionNCapasMaven/Configuration/SecurityConfiguration.java
+++ src/main/java/com/digis01/DAraizaProgramacionNCapasMaven/Configuration/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,21 +30,33 @@ public class SecurityConfiguration {
     }
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(configurer -> configurer
-        .requestMatchers("/usuario/**")
-        .hasAnyRole("Administrador", "Editor", "Usuario Estandar", "Visor", "Invitado")
-        .requestMatchers("/css/**", "/js/**", "/images/**", "/login").permitAll()
-        .anyRequest().authenticated())
-                
-        .formLogin(form -> form 
-         .loginPage("/login")
-        .loginProcessingUrl("/login")
-        .defaultSuccessUrl("/usuario")
-         .failureUrl("/login?error=true")
-        )
-        .userDetailsService(userDetailJPA);
-        
-        return http.build();
+//        http.authorizeHttpRequests(configurer -> configurer
+//        .requestMatchers("/usuario/**")
+//        .hasAnyRole("Administrador", "Editor", "Usuario Estandar", "Visor", "Invitado")
+//        .requestMatchers("/css/**", "/js/**", "/images/**", "/login").permitAll()
+//        .anyRequest().authenticated())
+//                
+//        .formLogin(form -> form 
+//         .loginPage("/login")
+//        .loginProcessingUrl("/login")
+//        .defaultSuccessUrl("/usuario")
+//         .failureUrl("/login?error=true")
+//        )
+//        .userDetailsService(userDetailJPA);
+//        
+//        return http.build();
+
+    return http
+        .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/auth/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    )
+            .build();
+    
     }
     
     @Bean
---
+++ src/main/java/com/digis01/DAraizaProgramacionNCapasMaven/RestController/AuthRestController.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.digis01.DAraizaProgramacionNCapasMaven.RestController;
+
+import com.digis01.DAraizaProgramacionNCapasMaven.Service.JwtService;
+import com.digis01.DAraizaProgramacionNCapasMaven.Service.UserDetailJPAService;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Dilan
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthRestController {
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailJPAService userDetailsService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> loginRequest) {
+        
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.get("email"), 
+                loginRequest.get("password")
+            )
+        );
+
+        UserDetails user = userDetailsService.loadUserByUsername(loginRequest.get("email"));
+        // si todo fue correcto
+        String token = jwtService.generateToken(user);
+        // api key 
+        Map<String, Object> map = new HashMap<>();
+        map.put("Key", token);
+        return ResponseEntity.ok(map);
+    }
+}
