48599108082a5b7d2cd30837c2cc7767ed4f3b94
Dilan <Dilan@DESKTOP-12CB95S>
2026-03-31 14:30:02 -0600

    Test

--- src/test/java/com/digis01/DAraizaProgramacionNCapasMaven/DAraizaProgramacionNCapasMavenApplicationTests.java
+++ src/test/java/com/digis01/DAraizaProgramacionNCapasMaven/DAraizaProgramacionNCapasMavenApplicationTests.java
@@ -1,13 +1,52 @@
 package com.digis01.DAraizaProgramacionNCapasMaven;
 
+import com.digis01.DAraizaProgramacionNCapasMaven.Configuration.DAO.UsuarioDAOJPAImplementation;
+import com.digis01.DAraizaProgramacionNCapasMaven.JPA.Result;
+import com.digis01.DAraizaProgramacionNCapasMaven.JPA.Usuario;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DAraizaProgramacionNCapasMavenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	   @Autowired
+    private UsuarioDAOJPAImplementation usuarioDAOJPAImplementation;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    void GetAll() {
+
+        Result result = usuarioDAOJPAImplementation.GetAll();
+
+        Assertions.assertTrue(result.correct);
+        Assertions.assertNotNull(result.objects);
+        Assertions.assertEquals(4, result.objects.size());
+
+    }
+
+    @Test
+    void Delete() {
+        Result result = usuarioDAOJPAImplementation.Delete(81);
+
+
+        Assertions.assertTrue(result.correct);
+        Assertions.assertNotNull(result);
+    }
+    
+@Test
+    void GetByIdDireccion() {
+        Result result = usuarioDAOJPAImplementation.DireccionGetById(81);
+
+
+        Assertions.assertTrue(result.correct);
+        Assertions.assertNotNull(result);
+    }
+    
+
 
 }
